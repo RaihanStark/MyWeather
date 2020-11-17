8b56b44e4951d47c7be381669efe447dc4d5caa6
Raihan Stark <raihanyudosaputra30@gmail.com>
2020-11-17 21:31:15 +0700

    change from groomet to bootstrap

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
 import React, { Component } from "react";
-import {
-  Box,
-  Button,
-  Collapsible,
-  Heading,
-  Grommet,
-  Layer,
-  ResponsiveContext,
-  Footer,
-  Anchor,
-  Text,
-} from "grommet";
-import { FormClose, Notification } from "grommet-icons";
+
+import { Container, Row, Col } from "reactstrap";
 
 import ForecastPanel from "./containers/ForecastPanel/ForecastPanel";
+import Layout from "./hoc/Layout/Layout";
+import styled from "styled-components";
 
-const theme = {
-  global: {
-    colors: {
-      brand: "#228BE6",
-      grey: "#F6F6F7",
-    },
-    font: {
-      family: "Poppins",
-      size: "14px",
-      height: "20px",
-    },
-  },
-};
 class App extends Component {
   render() {
     return (
-      <Grommet theme={theme} full>
-        <Box fill background="grey">
-          <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
-            <Box flex align="center" justify="center">
-              <ForecastPanel />
-            </Box>
-          </Box>
-        </Box>
-      </Grommet>
+      <Layout>
+        <ForecastPanel />
+      </Layout>
     );
   }
 }
---
+++ src/hoc/Layout/Layout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import styled from "styled-components";
+
+const DashboardCol = styled(Col)`
+  background-color: #f6f6f7;
+  height: 100vh;
+`;
+
+function Layout(props) {
+  return (
+    <Container fluid>
+      <Row noGutters>
+        <Col md="2"></Col>
+        <DashboardCol md="10">{props.children}</DashboardCol>
+      </Row>
+    </Container>
+  );
+}
+
+export default Layout;
